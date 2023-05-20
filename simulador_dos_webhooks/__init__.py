c5b8623808c218a48e4f6fcfaaa14066254e7197
Alisson D. S <108025321+Dhytm@users.noreply.github.com>
2023-05-20 11:53:15 -0300

    update __init__.py

--- simulador_dos_webhooks/__init__.py
+++ simulador_dos_webhooks/__init__.py
@@ -1,26 +1,42 @@
 from flask import Flask
+import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+
 
 app = Flask(__name__)
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 database_uri = 'sqlite:///' + os.path.join(current_dir, 'webhook.db')
 
-app.config['SECRET_KEY'] = '38cebh8gfj61k6f06jb3f57421d530t1'
 
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = database_uri  # 'sqlite:///webhook.db'
 
+if os.getenv("SECRET_KEY"):
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+else:
+    app.config['SECRET_KEY'] = '38cebh8gfj61k6f06jb3f57421d530t1'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+
+from simulador_dos_webhooks import models
+
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("DATABASE CRIADO!")
 
 from simulador_dos_webhooks import routes
